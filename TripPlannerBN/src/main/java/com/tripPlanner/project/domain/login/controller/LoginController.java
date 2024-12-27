4b65d001b92921c4619a185bb588609d9ab21af0
junyoung6 <your_email@example.com>
2024-12-27 11:30:22 +0900

    .

--- TripPlannerBN/src/main/java/com/tripPlanner/project/domain/login/controller/LoginController.java
+++ TripPlannerBN/src/main/java/com/tripPlanner/project/domain/login/controller/LoginController.java
@@ -12,16 +12,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000") //리액트 도메인 허가
 public class LoginController {
 
     private final LoginService loginService;
