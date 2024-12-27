17f5d2909a64089add61037a8e1145c3c5e289bf
junyoung6 <your_email@example.com>
2024-12-27 13:23:57 +0900

    .

--- src/main/java/com/tripPlanner/project/commons/SecurityConfig.java
+++ src/main/java/com/tripPlanner/project/commons/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.tripPlanner.project.commons.config;
+package com.tripPlanner.project.commons;
 
 import com.tripPlanner.project.domain.login.auth.handler.CustomLogoutHandler;
 import com.tripPlanner.project.domain.login.auth.handler.Oauth2LoginSuccessHandler;
